600ddeaeff1f6c0a9f66b02865001ab4d196de43
qizhang01 <zhishui120@sina.com>
2022-05-11 21:47:33 +0800

    第一次提交

---
+++ src/config/myCarouselList.config.js
@@ -0,0 +1,20 @@
+export default [
+	{
+		cover: '/static/img/1.jpg',
+		id: 1,
+		title: 1,
+		sort: 1
+	},
+	{
+		cover: '/static/img/2.jpg',
+		id: 2,
+		title: 2,
+		sort: 2
+	},
+	{
+		cover: '/static/img/3.jpg',
+		id: 3,
+		title: 3,
+		sort: 3
+	},
+]
